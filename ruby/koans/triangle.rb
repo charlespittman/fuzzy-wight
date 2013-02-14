65b0c07e0e3455fea86cbcaed1e9ee6d24cc3de6
Charles Pittman <charles.pittman@gmail.com>
2013-02-14 18:34:12 -0500

    Completed more koans

--- ruby/koans/triangle.rb
+++ ruby/koans/triangle.rb
@@ -14,8 +14,16 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  matches = 0
+  matches += 1 if a == b
+  matches += 1 if b == c
+  matches
 end
+
+p triangle(1,2,3)
+p triangle(2,2,3)
+p triangle(3,2,3)
+p triangle(3,3,3)
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
