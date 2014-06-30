f6f119c432d61cf54a014d901c5841db57184106
Charles Pittman <charles.pittman@gmail.com>
2014-06-30 17:05:37 -0400

    Added Affine cipher

---
+++ py/hackingciphers/affineCipher.py
@@ -0,0 +1,86 @@
+# Affine Cipher
+
+import sys
+import pyperclip
+import cryptomath
+import random
+import string
+
+# Set of symbols that can be encrypted (includes whitespace)
+SYMBOLS = string.printable
+
+
+def main():
+    #myMessage = """"A computer would deserve to be called intelligent if it could deceive a human into believing that it was human." -Alan Turing"""
+    myMessage = """0,vNZX!&%P#v(Z&WOvOP$P#'Pv%ZvMPvNLWWPOvTY%PWWTRPY%vTQvT%vNZ&WOvOPNPT'PvLvS&XLYvTY%ZvMPWTP'TYRv%SL%vT%v(L$vS&XLYc0vb,WLYv}&#TYR"""
+    myKey = 10137
+    #myMode = 'encrypt'  # encrypt or decrypt
+    myMode = 'decrypt'
+
+    if myMode == 'encrypt':
+        translated = encryptMessage(myKey, myMessage)
+    elif myMode == 'decrypt':
+        translated = decryptMessage(myKey, myMessage)
+    print('Key: %s' % (myKey))
+    print('%sed text:' % (myMode.title()))
+    print(translated)
+    pyperclip.copy(translated)
+    print('Full %sed text copied to clipboard.' % (myMode))
+
+
+def getKeyParts(key):
+    keyA = key // len(SYMBOLS)
+    keyB = key % len(SYMBOLS)
+    return (keyA, keyB)
+
+
+def checkKeys(keyA, keyB, mode):
+    if keyA == 1 and mode == 'encrypt':
+        sys.exit('The affine cipher becomes incredibly weak when keyA is 1.  Choose a different key.')
+    if keyB == 0 and mode == 'encrypt':
+        sys.exit('The affine cipher becomes incredibly weak when keyB is 0.  Choose a different key.')
+    if keyA < 0 or keyB < 0 or keyB > len(SYMBOLS) - 1:
+        sys.exit('KeyA must be > 0 and 0 < keyB < %s.' % (len(SYMBOLS) - 1))
+    if cryptomath.gcd(keyA, len(SYMBOLS)) != 1:
+        sys.exit('KeyA (%s) and the symbol set size (%s) must be relatively prime.  Choose a different key.' % (keyA, len(SYMBOLS)))
+
+
+def encryptMessage(key, message):
+    keyA, keyB = getKeyParts(key)
+    checkKeys(keyA, keyB, 'encrypt')
+
+    ciphertext = ''
+    for symbol in message:
+        if symbol in SYMBOLS:
+            symIndex = SYMBOLS.find(symbol)
+            ciphertext += SYMBOLS[(symIndex * keyA + keyB) % len(SYMBOLS)]
+        else:
+            ciphertext += symbol
+    return ciphertext
+
+
+def decryptMessage(key, message):
+    keyA, keyB = getKeyParts(key)
+    checkKeys(keyA, keyB, 'decrypt')
+    plaintext = ''
+    modInverseOfKeyA = cryptomath.findModInverse(keyA, len(SYMBOLS))
+
+    for symbol in message:
+        if symbol in SYMBOLS:
+            symIndex = SYMBOLS.find(symbol)
+            plaintext += SYMBOLS[(symIndex - keyB) * modInverseOfKeyA % len(SYMBOLS)]
+        else:
+            plaintext += symbol
+    return plaintext
+
+
+def getRandomKey():
+    while True:
+        keyA = random.randint(2, len(SYMBOLS))
+        keyB = random.randint(2, len(SYMBOLS))
+        if cryptomath.gcd(keyA, len(SYMBOLS)) == 1:
+            return keyA * len(SYMBOLS) + keyB
+
+
+if __name__ == '__main__':
+    main()
